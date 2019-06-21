122d911c0e6984a339e163570e3c3c637f9dd95f
Mat1989Git <mateuszfilipowicz@yahoo.com>
2019-06-21 01:56:36 +0100

    updated headline2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hi Munchkin </h1>
+        <h1> :**** Hi Munchkin :***** </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
